e2cbd0ffdb15567c607e458908b382023c8056f9
Mariana Ortiz <ortizmariana@uninorte.edu.co>
2026-03-23 23:07:26 -0500

    pasar de csv a json

--- newUsersMenu.py
+++ newUsersMenu.py
@@ -3,8 +3,7 @@ import pygame
 import sys
 from Button import Button
 from InputBox import InputBox
-from Repositories.Profile_repository import ProfileRepository
-
+from Repositories.Profile_repository import ProfileRepository  
 
 repo = ProfileRepository()
 
@@ -24,7 +23,7 @@ def runNewUsersMenu(screen, events, bg):
         runNewUsersMenu.backButton = Button("Atras", 200, 60, (WIDTH-200 , HEIGHT-150), runNewUsersMenu.button_font)
         runNewUsersMenu.saveButton = Button("Guardar", 200, 60, (WIDTH//2 , 500), runNewUsersMenu.button_font)
 
-        #Inputs
+        # Inputs
         runNewUsersMenu.nameInputBox = InputBox(WIDTH//2-200, 400, 400, 60, runNewUsersMenu.button_font)
         
         # Estado
@@ -37,46 +36,40 @@ def runNewUsersMenu(screen, events, bg):
     title_font = runNewUsersMenu.title_font
     button_font = runNewUsersMenu.button_font
     
-    #Manejo de eventos
+    # Manejo de eventos
     for event in events:
-        result=nameInputBox.handle_event(event)
+        nameInputBox.handle_event(event)
         if event.type == pygame.QUIT:
             return 0
         if backButton.handle_event(event):
             runNewUsersMenu.action = "back"
-            
-
         if saveButton.handle_event(event):
             name = nameInputBox.text.strip()
             if name != "":
+                # Crear perfil en JSON
                 newId = repo.get_next_id()
                 repo.save_profile(newId, name, 0, 0)
                 print(f"Created {newId} for {name}")
 
                 nameInputBox.text = ""
-                
             else:
                 print("Name cannot be empty")
-
             
     mouse_pos = pygame.mouse.get_pos()
 
-    # Limpa 
+    # Limpiar pantalla
     screen.blit(bg, (0, 0))
 
     # Title
     title = title_font.render("Creacion de Usuario", True, (255, 60, 200))
     title_rect = title.get_rect(center=(WIDTH//2, 150))
-    for i in range(6, 0, -1):
-        glow = title_font.render("Creacion de Usuario", True, (255, 20, 147))
-        screen.blit(glow, title_rect)
+    screen.blit(title, title_rect)
         
-    title = button_font.render("Ingrese su nombre", True, (95, 38, 105))
-    title_rect = title.get_rect(center=(WIDTH//2, 360))
-    for i in range(6, 0, -1):
-        glow = button_font.render("Ingrese su nombre", True, (255, 255, 255))
-        screen.blit(glow, title_rect)    
+    prompt = button_font.render("Ingrese su nombre", True, (95, 38, 105))
+    prompt_rect = prompt.get_rect(center=(WIDTH//2, 360))
+    screen.blit(prompt, prompt_rect)
     
+    # Actualizar botones e inputs
     backButton.update(mouse_pos)       
     saveButton.update(mouse_pos)
     
@@ -88,8 +81,7 @@ def runNewUsersMenu(screen, events, bg):
         runNewUsersMenu.action = None
         return 4
     
-    
-    # Actualizar 
+    # Actualizar pantalla
     pygame.display.flip()
     
     return 5
